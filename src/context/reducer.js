64fde2cd829c18ae9f1a869a0d2283745e87ddf4
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2023-04-08 04:07:43 +0300

    react toastify, minor adjustments

--- src/context/reducer.js
+++ src/context/reducer.js
@@ -1,19 +1,27 @@
 const reducer = (state, action) => {
   if (action.type === "CLEAR_CART") {
-    // cleart cart fonksiyonu için amount verileri sıfırlanacak
+    // cleart cart fonksiyonu için amount verileri sıfırlandı, düzenlendi.
+    let temp = state.cart.map((item) => {
+      item.amount = 1
+      return item;
+    });
+    state.cart = temp
     return { ...state, cart: [] };
   }
-  // same key erroru için düzenleme yapılacak
+  // decrease ve increase işlemleri sonrası add item düzgün çalışmıyor
   if (action.type === "ADD_ITEM") {
     if (state.cart.includes(action.payload.item)) {
-      console.log(action.payload.id);
-      state.cart.map((item) => {
-        if (item.id === action.payload.id) item.amount += 1;
+      let temp = state.cart.map((item) => {
+        if (item.id === action.payload.id) {
+          item.amount += 1;
+          return item;
+        } 
+        return item;
       });
 
-      return { ...state };
+      return { ...state, cart : temp };
     } else {
-      return { ...state, cart: [...state.cart, action.payload.item] };
+    return { ...state, cart: [...state.cart, action.payload.item] };
     }
   }
   if (action.type === "DELETE_ITEM") {
@@ -33,7 +41,7 @@ const reducer = (state, action) => {
     });
     return { ...state, cart: temp };
   }
-
+// decrease fonksyionu için amount miktarları düzenlenecek
   if (action.type === "DECREASE") {
     let temp = state.cart
       .map((item) => {
